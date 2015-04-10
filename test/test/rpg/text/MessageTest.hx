f5922b158a4dad45f069cae6d008e6f4182ae828
Kevin Leung <kevinresol@gmail.com>
2015-04-10 22:31:31 +0800

    More tests

--- test/test/rpg/text/MessageTest.hx
+++ test/test/rpg/text/MessageTest.hx
@@ -57,4 +57,45 @@ class MessageTest
 		Assert.isTrue(section.length == 11);
 	}
 	
+	@Test
+	public function testInstantDisplay():Void
+	{
+		var message = new Message("This is />some/< texts.");
+		Assert.isTrue(message.lines.length == 1);
+		
+		var line = message.lines[0];
+		Assert.isTrue(line.text == "This is some texts.");		
+		Assert.isTrue(line.speed.length == 3);
+		
+		var section = line.speed[0];
+		Assert.isTrue(Type.enumEq(section.attribute, SSpeed(5))); // default speed is 5
+		Assert.isTrue(section.startIndex == 0);
+		Assert.isTrue(section.length == 8);
+		
+		section = line.speed[1];
+		Assert.isTrue(Type.enumEq(section.attribute,SInstantDisplay));
+		Assert.isTrue(section.startIndex == 8);
+		Assert.isTrue(section.length == 4);
+		
+		section = line.speed[2];
+		Assert.isTrue(Type.enumEq(section.attribute, SSpeed(5)));
+		Assert.isTrue(section.startIndex == 12);
+		Assert.isTrue(section.endIndex == 6);
+	}
+	
+	@Test
+	public function testLine():Void
+	{
+		var message = new Message("/S[1]This is /S[2]some \ntexts.");
+		Assert.isTrue(message.lines.length == 2);
+		
+		var line = message.lines[0];
+		Assert.isTrue(line.text == "This is some ");
+		Assert.isTrue(line.speed.length == 2);
+		
+		line = message.lines[1];
+		Assert.isTrue(line.text == "texts.");
+		Assert.isTrue(line.speed.length == 1);
+	}
+	
 }
