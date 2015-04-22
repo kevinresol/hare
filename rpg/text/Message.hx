114b7780f7db592a38c515aea33bdfbb9a148c74
Kevin Leung <kevinresol@gmail.com>
2015-04-22 12:20:19 +0800

    Encapsulate the parsing logic inside Message class

--- rpg/text/Message.hx
+++ rpg/text/Message.hx
@@ -1,5 +1,4 @@
 package rpg.text;
-import rpg.text.Message.Sections;
 
 /**
  * ...
@@ -9,23 +8,134 @@ class Message
 {
 	public var lines:Array<Line>;
 	
+	private static var colorCodes:ReadOnlyArray<Int> = [0xFF0000, 0x00FF00, 0x0000FF]; //System Color Codes
+	
 	public function new(rawText:String):Void
 	{
 		lines = [];
-		var rawLines = rawText.split("\n");
-		for(line in rawLines)
+		
+		var splitedRawText = rawText.split("\n");
+		
+		// default attributes:
+		var fontColor = colorCodes[0];
+		var speed = SSpeed(5);
+		var prevSpeed = null; // for instant display to get back to prev speed
+		var bold = false;
+		var italic = false;
+		
+		
+		for (i in 0...splitedRawText.length)
 		{
-			lines.push(new CommandParser(line).parseMessage());
+			var t = splitedRawText[i];
+			var line = new Line();
+			lines.push(line);
+			
+			var pos = 0; // pos of rawText
+			var startIndex = pos; // pos of the text with commands removed
+			
+			while (pos < t.length)
+			{
+				// get the pos of next command
+				var nextPos = t.indexOf("/", pos);
+				
+				// next command not found, set it to the end of line
+				if (nextPos == -1)
+					nextPos = t.length;
+				
+				// push new sections to current line
+				if (pos != nextPos)
+				{
+					var sectionText = t.substring(pos, nextPos);
+					var sectionLength = sectionText.length;
+					var endIndex = startIndex + sectionLength - 1;
+					
+					line.text += sectionText;
+					
+					line.speed.push(new Section(speed, startIndex, endIndex));
+					line.fontColor.push(new Section(fontColor, startIndex, endIndex));
+					//TODO: line.fontSize.push(new Section(fontColor, startIndex, endIndex));
+					//TODO: line.fontName.push(new Section(fontColor, startIndex, endIndex));
+					line.bold.push(new Section(bold, startIndex, endIndex));
+					line.italic.push(new Section(italic, startIndex, endIndex));
+					//TODO: line.borderColor.push(new Section(fontColor, startIndex, endIndex));
+					
+					startIndex += sectionLength;
+				}
+				
+				// reached the end of current line
+				if(nextPos == t.length) break;
+				
+				// parse next command
+				var parseParam = function():String // helper function for getting param for commands like "/C[1]"
+				{
+					pos = t.indexOf("]", nextPos);
+					return t.substr(nextPos + 3, pos - nextPos - 3);
+				}
+				
+				switch (t.charAt(nextPos + 1))
+				{
+					// "/S[n]"
+					case "S":
+						var param = parseParam();
+						speed = SSpeed(Std.parseInt(param));
+						pos++;
+						
+					// "/C[n]"
+					case "C":
+						var param = parseParam();
+						fontColor = colorCodes[Std.parseInt(param)];
+						pos++;
+						
+					// "/c[hex]"
+					case "c":
+						var param = parseParam();
+						fontColor = Std.parseInt('0x$param');
+						pos++;
+						
+					// "/>"
+					case ">":
+						prevSpeed = speed;
+						speed = SInstantDisplay;
+						pos = nextPos + 2;
+						
+					// "/<"
+					case "<":
+						if(speed == SInstantDisplay && prevSpeed != null) // if there is no "/>" before "/<", need to display some error?
+							speed = prevSpeed;
+						pos = nextPos + 2;
+						
+					case "f":
+						switch (t.charAt(nextPos + 2)) 
+						{
+							
+							// "/fb"
+							case "b":
+								bold = !bold;
+								pos = nextPos + 3;
+								
+							// "/fi"
+							case "i":
+								italic = !italic;
+								pos = nextPos + 3;
+								
+							default:
+								// error
+						}
+						
+					default:
+						// error
+						
+				}
+			}
 		}
 	}
 }
  
 class Line
 {
-	public var text:String;
+	public var text:String = "";
 	
 	public var speed:Sections<SpeedAttribute>;
-	
 	public var fontColor:Sections<Int>;
 	public var fontSize:Sections<Int>; //TODO
 	public var fontName:Sections<String>; //TODO
@@ -33,15 +143,60 @@ class Line
 	public var italic:Sections<Bool>; //TODO
 	public var borderColor:Sections<Int>; //TODO
 	
-	public function new(text:String,speed:Sections<SpeedAttribute>,fontColor:Sections<Int>) 
+	public function new() 
 	{
-		this.text = text;
-		this.speed = speed;
-		this.fontColor = fontColor;
+		speed       = [];
+		fontColor   = [];
+		fontSize    = [];
+		fontName    = [];
+		bold        = [];
+		italic      = [];
+		borderColor = [];
+	}
+	
+	public function toString():String
+	{
+		return '\ntext:$text\nfontColor:$fontColor\nspeed:$speed\nbold:$bold\nitalic:$italic';
+	}
+	
+	@:allow(rpg.text.Message)
+	private function mergeSections():Void
+	{
+		
 	}
 }
 
-typedef Sections<T> = Array<Section<T>>;
+@:forward(length)
+abstract Sections<T>(Array<Section<T>>) from Array<Section<T>>
+{
+	// override the push function, merge the to-be-pushed section with the prev section if the attribute are the same
+	public function push(section:Section<T>)
+	{
+		if (this.length > 0)
+		{
+			var prevSection = this[this.length - 1];
+			if (attributeEquals(prevSection.attribute, section.attribute))
+			{
+				prevSection.endIndex = section.endIndex;
+				return this.length;
+			}
+		}
+		return this.push(section);
+	}
+	
+	@:arrayAccess
+	public inline function get(i:Int) return this[i];
+	
+	// compare the attributes
+	private function attributeEquals(attr1:T, attr2:T):Bool
+	{
+		if (Reflect.isEnumValue(attr1))
+			return Type.enumEq(attr1, attr2);
+		else
+			return attr1 == attr2;
+	}
+}
+
 class Section<T>
 {
 	public var attribute:T;
@@ -56,6 +211,11 @@ class Section<T>
 		this.endIndex = endIndex;
 	}
 	
+	public function toString():String
+	{
+		return 'Section($attribute, $startIndex, $endIndex)';
+	}
+	
 	private inline function get_length():Int
 	{
 		return endIndex - startIndex + 1;
@@ -66,4 +226,10 @@ enum SpeedAttribute
 {
 	SSpeed(speed:Int);	
 	SInstantDisplay;
+}
+
+abstract ReadOnlyArray<T>(Array<T>) from Array<T>
+{
+    public inline function new(arr) this = arr;
+    @:arrayAccess public inline function get(index) return this[index];
 }
--- test/test/rpg/text/MessageTest.hx
+++ test/test/rpg/text/MessageTest.hx
@@ -2,12 +2,12 @@ package rpg.text;
 
 import massive.munit.Assert;
 import rpg.text.Message;
-import rpg.text.CommandParser;
 
 /**
  * ...
  * @author Kevin
  */
+@:access(rpg.text.Message)
 class MessageTest
 {
 
@@ -38,7 +38,6 @@ class MessageTest
 	}
 	
 	@Test
-	@:access(rpg.text.CommandParser)
 	public function testColor():Void
 	{
 		var message = new Message("/C[1]This is /C[2]some texts.");
@@ -49,12 +48,12 @@ class MessageTest
 		Assert.isTrue(line.fontColor.length == 2);
 		
 		var section = line.fontColor[0];
-		Assert.isTrue(section.attribute == CommandParser.colorCodes[1]);
+		Assert.isTrue(section.attribute == Message.colorCodes[1]);
 		Assert.isTrue(section.startIndex == 0);
 		Assert.isTrue(section.endIndex == 7);
 		
 		section = line.fontColor[1];
-		Assert.isTrue(section.attribute == CommandParser.colorCodes[2]);
+		Assert.isTrue(section.attribute == Message.colorCodes[2]);
 		Assert.isTrue(section.startIndex == 8);
 		Assert.isTrue(section.length == 11);
 	}
@@ -128,6 +127,43 @@ class MessageTest
 	}
 	
 	@Test
+	public function testInstantDisplayMix():Void
+	{
+		var message = new Message("This is />some /C[1]more/< texts.");
+		Assert.isTrue(message.lines.length == 1);
+		
+		var line = message.lines[0];
+		Assert.isTrue(line.text == "This is some more texts.");		
+		Assert.isTrue(line.speed.length == 3);
+		Assert.isTrue(line.fontColor.length == 2);
+		
+		var section = line.fontColor[0];
+		Assert.isTrue(section.attribute == Message.colorCodes[0]); // default color
+		Assert.isTrue(section.startIndex == 0);
+		Assert.isTrue(section.endIndex == 12);
+		
+		section = line.fontColor[1];
+		Assert.isTrue(section.attribute == Message.colorCodes[1]);
+		Assert.isTrue(section.startIndex == 13);
+		Assert.isTrue(section.length == 11);
+		
+		var section = line.speed[0];
+		Assert.isTrue(Type.enumEq(section.attribute, SSpeed(5))); // default speed is 5
+		Assert.isTrue(section.startIndex == 0);
+		Assert.isTrue(section.length == 8);
+		
+		var section = line.speed[1];
+		Assert.isTrue(Type.enumEq(section.attribute, SInstantDisplay));
+		Assert.isTrue(section.startIndex == 8);
+		Assert.isTrue(section.length == 9);
+		
+		section = line.speed[2];
+		Assert.isTrue(Type.enumEq(section.attribute, SSpeed(5)));
+		Assert.isTrue(section.startIndex == 17);
+		Assert.isTrue(section.length == 7);
+	}
+	
+	@Test
 	public function testLine():Void
 	{
 		var message = new Message("/S[1]This is /S[2]some \ntexts.");
@@ -145,7 +181,6 @@ class MessageTest
 	
 	
 	@Test
-	@:access(rpg.text.CommandParser)
 	public function testMixedCommands():Void
 	{
 		var message = new Message("/C[1]This /S[1]is /C[2]some texts.");
@@ -157,12 +192,12 @@ class MessageTest
 		Assert.isTrue(line.speed.length == 2);
 		
 		var section = line.fontColor[0];
-		Assert.isTrue(section.attribute == CommandParser.colorCodes[1]);
+		Assert.isTrue(section.attribute == Message.colorCodes[1]);
 		Assert.isTrue(section.startIndex == 0);
 		Assert.isTrue(section.endIndex == 7);
 		
 		section = line.fontColor[1];
-		Assert.isTrue(section.attribute == CommandParser.colorCodes[2]);
+		Assert.isTrue(section.attribute == Message.colorCodes[2]);
 		Assert.isTrue(section.startIndex == 8);
 		Assert.isTrue(section.length == 11);
 		
