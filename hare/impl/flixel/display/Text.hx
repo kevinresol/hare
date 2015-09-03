834754392161665c1fbf60ee70105dc63e6e6164
Kevin Leung <kevinresol@gmail.com>
2015-09-03 21:39:19 +0800

    also test building the samples in CI

--- hare/impl/flixel/display/Text.hx
+++ hare/impl/flixel/display/Text.hx
@@ -10,7 +10,7 @@ import openfl.Assets;
 class Text extends FlxText
 {
 
-	public function new(X:Float=0, Y:Float=0, FieldWidth:Float=0, ?Text:String, Size:Float=8, EmbeddedFont:Bool=true) 
+	public function new(X:Float=0, Y:Float=0, FieldWidth:Float=0, ?Text:String, Size:Int=8, EmbeddedFont:Bool=true) 
 	{
 		super(X, Y, FieldWidth, Text, Size, EmbeddedFont);
 		
--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@ haxelib dev hare .
 (cd /home/travis/haxe/lib/hxcpp/git/project && neko build.n clean && neko build.n)
 haxelib run lime rebuild lua linux -64 -clean
 cd test && haxelib run munit test -neko && haxelib run munit test -cpp
+cd ../samples/flixel && haxelib run lime build neko
